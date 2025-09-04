8f756fe833ec8e7214d80e70e2f37ee2bc088712
LeHai1004 <email_github_cua_ban@gmail.com>
2025-09-04 11:11:23 +0700

    lan2

--- bai1.cpp
+++ bai1.cpp
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
-
-bool isPrime(int n) {
-    if (n < 2) return false;
-    for (int i = 2; i < n; i++) {  // ki?m tra t? 2 t?i n-1 (r?t ch?m)
-        if (n % i == 0) return false;
-    }
-    return true;
-}
+#include <stdlib.h>
 
 int main() {
     int n;
     printf("Nhap n: ");
     scanf("%d", &n);
 
-    int sum = 0;
-    for (int i = 2; i <= n; i++) {
-        if (isPrime(i)) {
-            sum += i;
+    bool *prime = malloc((n+1) * sizeof(bool));
+    for (int i = 0; i <= n; i++) prime[i] = true;
+
+    prime[0] = prime[1] = false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                prime[j] = false;
+            }
         }
     }
 
+    int sum = 0;
+    for (int i = 2; i <= n; i++) {
+        if (prime[i]) sum += i;
+    }
+
     printf("Tong cac so nguyen to <= %d la: %d\n", n, sum);
+
+    free(prime);
     return 0;
 }
 
